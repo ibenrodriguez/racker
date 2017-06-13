56d78e0c59e5b3006b4dc1f01a8f01a61aed178a
Iben Rodriguez <irodrigu@nps.edu>
2017-06-12 21:48:19 -0700

    updated with variables
    updated with variables for sites, hypervisors, vms, etc…

--- scripts/vsgtests.pl
+++ scripts/vsgtests.pl
@@ -2,23 +2,30 @@
 $now_string = localtime;  # e.g., " Mon Oct 21 15:25:14 2013."
 print "Begin - $now_string.\n";
 $row = 1;
-
+$sitecount = 4;
+$hypervisorpersite = 2;
+$vmperhypervisor = 2;
+$vnicpervm = 2;
+$tenantcount = 2;
+$vmcount = $sitecount * $hypervisorpersite * $vmperhypervisor * $tenantcount;
 
 print "row,hypervisor,tenant,vm,vnic\n";
-for ($vm = 0; $vm <=12; $vm++) {
-  for (my $hypervisor = 1; $hypervisor <= 3; $hypervisor++) {
-       for ($tenant = 1; $tenant <= 2; $tenant++) {
+for ($vm = 0; $vm <= $vmcount; $vm++) {
+  for ($site = 1; $site <= $sitecount; $site++) {
+  for (my $hypervisor = 1; $hypervisor <= $hypervisorpersite; $hypervisor++) {
+       for ($tenant = 1; $tenant <= $tenantcount; $tenant++) {
          $vm++;
-         for ($vnic = 1; $vnic <= 2; $vnic++) {
-           print "row=",$row," hypervisor=",$hypervisor," T=",$tenant," VM=",$vm," VNIC=",$vnic,"\n";$row++;
+         for ($vnic = 1; $vnic <= $vnicpervm; $vnic++) {
+           print "row=",$row," site=",$site," hypervisor=",$hypervisor," T=",$tenant," VM=",$vm," VNIC=",$vnic,"\n";$row++;
          }
          $vm++;
          for ($vnic = 1; $vnic <= 2; $vnic++) {
-           print "row=",$row," hypervisor=",$hypervisor," T=",$tenant," VM=",$vm," VNIC=",$vnic,"\n";$row++;
+           print "row=",$row," site=",$site," hypervisor=",$hypervisor," T=",$tenant," VM=",$vm," VNIC=",$vnic,"\n";$row++;
          }
        }
+     }
    }
- }
+}
 $now_string = localtime;
 print "Done - $now_string.\n";
 # end
